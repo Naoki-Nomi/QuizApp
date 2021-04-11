5f688b3562e91a17bfa4154e776a20a1022669e5
Naoki-Nomi <n.naoki.116@gmail.com>
2021-04-11 17:05:35 +0900

    [Update]クイズを設定

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -8,17 +8,102 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    @IBOutlet weak var quizNumber: UILabel!
+    @IBOutlet weak var quizContent: UITextView!
+    @IBOutlet weak var quizAnswer1: UIButton!
+    @IBOutlet weak var quizAnswer2: UIButton!
+    @IBOutlet weak var quizAnswer3: UIButton!
+    @IBOutlet weak var quizAnswer4: UIButton!
+    @IBOutlet weak var JudgeImageView: UIImageView!
+    @IBOutlet weak var QuizExplanation: UITextView!
+    @IBOutlet weak var MoveToNextQuiz: UIButton!
     
+    
+    var csvArray = [String]()
+    var eachArray = [String]()
     var selectLevel = 0
+    var quizCount = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        csvArray = loadCSV(fileName: "quiz\(selectLevel)")
+        eachArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumber.text = "第\(quizCount+1)問"
+        quizContent.text = eachArray[0]
+        quizAnswer1.setTitle(eachArray[2], for: .normal)
+        quizAnswer2.setTitle(eachArray[3], for: .normal)
+        quizAnswer3.setTitle(eachArray[4], for: .normal)
+        quizAnswer4.setTitle(eachArray[5], for: .normal)
+        QuizExplanation.isHidden = true
+        MoveToNextQuiz.isHidden = true
         // Do any additional setup after loading the view.
     }
     
     @IBAction func btnAction(sender: UIButton){
-        print(sender.tag)
+        if sender.tag == Int(eachArray[1]) {
+            JudgeImageView.image = UIImage(named: "correct")
+            
+        }
+        else {
+            JudgeImageView.image = UIImage(named: "incorrect")
+        }
+        if quizCount < csvArray.count - 1 {
+            MoveToNextQuiz.setTitle("次の問題に進む", for: .normal)
+        } else {
+            MoveToNextQuiz.setTitle("結果を確認する", for: .normal)
+        }
+        JudgeImageView.isHidden = false
+        QuizExplanation.isHidden = false
+        QuizExplanation.text = eachArray[6]
+        MoveToNextQuiz.isHidden = false
+        quizAnswer1.isHidden = true
+        quizAnswer2.isHidden = true
+        quizAnswer3.isHidden = true
+        quizAnswer4.isHidden = true
+    }
+    
+    @IBAction func NextQuiz(_ sender: Any) {
+        nextQuiz()
+        QuizExplanation.isHidden = true
+        MoveToNextQuiz.isHidden = true
+        JudgeImageView.isHidden = true
+        quizAnswer1.isHidden = false
+        quizAnswer2.isHidden = false
+        quizAnswer3.isHidden = false
+        quizAnswer4.isHidden = false
+    }
+    
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            var lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            lineChange = lineChange.replacingOccurrences(of: "\n\n", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー発生")
+        }
+        return csvArray
+    }
+    
+    func nextQuiz() {
+        quizCount += 1
+        if quizCount < csvArray.count {
+            csvArray = loadCSV(fileName: "quiz\(selectLevel)")
+            eachArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumber.text = "第\(quizCount+1)問"
+            quizContent.text = eachArray[0]
+            quizAnswer1.setTitle(eachArray[2], for: .normal)
+            quizAnswer2.setTitle(eachArray[3], for: .normal)
+            quizAnswer3.setTitle(eachArray[4], for: .normal)
+            quizAnswer4.setTitle(eachArray[5], for: .normal)
+        } else {
+            self.performSegue(withIdentifier: "MoveToScoreVC", sender: nil)
+        }
     }
     
 
