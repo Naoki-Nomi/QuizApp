c4222edf0468f074ca307a81ad9add662a1b816f
Naoki-Nomi <n.naoki.116@gmail.com>
2021-04-12 14:30:35 +0900

    [Update]ボタンの枠線を追加

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startButton.layer.borderWidth = 2
+        startButton.layer.borderColor = UIColor.black.cgColor
         // Do any additional setup after loading the view.
     }
 
